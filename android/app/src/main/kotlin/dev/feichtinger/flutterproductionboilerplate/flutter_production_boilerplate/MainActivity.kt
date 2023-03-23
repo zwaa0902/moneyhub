1b383223393ee281fdf136fd43f1deeef29f4007
zwaa0902 <ngatt@safin.vn>
2023-03-23 10:46:49 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/dev/feichtinger/flutterproductionboilerplate/flutter_production_boilerplate/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.feichtinger.flutterproductionboilerplate.stoken
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/theme.dart
@@ -0,0 +1,300 @@
+import 'package:flutter/material.dart';
+
+/// Colors from Tailwind CSS (v3.0) - June 2022
+///
+/// https://tailwindcss.com/docs/customizing-colors
+///
+
+class AppColor {
+  AppColor._();
+
+  static const Color purple900 = Color(0xff4407AF);
+  static const Color purple800 = Color(0xff4D28C8);
+  static const Color purple700 = Color(0xff5436D3);
+  static const Color purple600 = Color(0xff5D43E0);
+  static const Color purple500 = Color(0xff624CEC);
+  static const Color purple400 = Color(0xff776BF2);
+  static const Color purple300 = Color(0xff8E89F6);
+  static const Color purple200 = Color(0xffAFABF8);
+  static const Color purple100 = Color(0xffCFCCFB);
+  static const Color purple50 = Color(0xffECEBFE);
+
+  static const Color indigo900 = Color(0xff001DB0);
+  static const Color indigo800 = Color(0xff1129BB);
+  static const Color indigo700 = Color(0xff2E2EC2);
+  static const Color indigo600 = Color(0xff4336CB);
+  static const Color indigo500 = Color(0xff4F3BD1);
+  static const Color indigo400 = Color(0xff6E57D9);
+  static const Color indigo300 = Color(0xff8B75E0);
+  static const Color indigo200 = Color(0xffAD9DE8);
+  static const Color indigo100 = Color(0xffCEC4F1);
+  static const Color indigo50 = Color(0xffECE7F9);
+
+  //Neutrals
+  static const Color neutrals1000 = Color(0xff1C1C1E);
+  static const Color neutrals900 = Color(0xff2C2C2E);
+  static const Color neutrals800 = Color(0xff3A3A3C);
+  static const Color neutrals700 = Color(0xff48484A);
+  static const Color neutrals600 = Color(0xff636366);
+  static const Color neutrals500 = Color(0xff8E8E93);
+  static const Color neutrals400 = Color(0xffAEAEB2);
+  static const Color neutrals300 = Color(0xffC7C7CC);
+  static const Color neutrals200 = Color(0xffD1D1D6);
+  static const Color neutrals100 = Color(0xffE5E5EA);
+  static const Color neutrals50 = Color(0xffF2F2F7);
+  static const Color neutrals0 = Color(0xffFFFFFF);
+
+//Semantic
+  static const Color pink900 = Color(0xff990745);
+  static const Color pink800 = Color(0xffBD1648);
+  static const Color pink700 = Color(0xffD11F4A);
+  static const Color pink600 = Color(0xffE6264C);
+  static const Color pink500 = Color(0xffF62C4E);
+  static const Color pink400 = Color(0xffFB4566);
+  static const Color pink300 = Color(0xffFF6581);
+  static const Color pink200 = Color(0xffFF91A5);
+  static const Color pink100 = Color(0xffFFBCC9);
+  static const Color pink50 = Color(0xffFFE5E9);
+
+  static const Color green900 = Color(0xff007114);
+  static const Color green800 = Color(0xff009424);
+  static const Color green700 = Color(0xff1DA92E);
+  static const Color green600 = Color(0xff3CBD39);
+  static const Color green500 = Color(0xff4DCE41);
+  static const Color green400 = Color(0xff6DD65F);
+  static const Color green300 = Color(0xff8BDE7D);
+  static const Color green200 = Color(0xffAEE8A4);
+  static const Color green100 = Color(0xffCFF1C8);
+  static const Color green50 = Color(0xffECFAE9);
+
+  static const Color yellow900 = Color(0xff990745);
+  static const Color yellow800 = Color(0xffBD1648);
+  static const Color yellow700 = Color(0xffD11F4A);
+  static const Color yellow600 = Color(0xffE6264C);
+  static const Color yellow500 = Color(0xffF62C4E);
+  static const Color yellow400 = Color(0xffFB4566);
+  static const Color yellow300 = Color(0xffFF6581);
+  static const Color yellow200 = Color(0xffFF91A5);
+  static const Color yellow100 = Color(0xffFFBCC9);
+  static const Color yellow50 = Color(0xffFFE5E9);
+
+  static const Color blue900 = Color(0xff990745);
+  static const Color blue800 = Color(0xffBD1648);
+  static const Color blue700 = Color(0xffD11F4A);
+  static const Color blue600 = Color(0xffE6264C);
+  static const Color blue500 = Color(0xffF62C4E);
+  static const Color blue400 = Color(0xffFB4566);
+  static const Color blue300 = Color(0xffFF6581);
+  static const Color blue200 = Color(0xffFF91A5);
+  static const Color blue100 = Color(0xffFFBCC9);
+  static const Color blue50 = Color(0xffFFE5E9);
+
+  //extend
+
+  static const Color cyan900 = Color(0xff006268);
+  static const Color cyan800 = Color(0xff008594);
+  static const Color cyan700 = Color(0xff0099AC);
+  static const Color cyan600 = Color(0xff00AEC7);
+  static const Color cyan500 = Color(0xff00BEDA);
+  static const Color cyan400 = Color(0xff00C8DF);
+  static const Color cyan300 = Color(0xff2FD2E6);
+  static const Color cyan200 = Color(0xff74E0EE);
+  static const Color cyan100 = Color(0xffADECF4);
+  static const Color cyan50 = Color(0xffDEF7FB);
+
+  static const Color red900 = Color(0xff006268);
+  static const Color red800 = Color(0xff008594);
+  static const Color red700 = Color(0xff0099AC);
+  static const Color red600 = Color(0xff00AEC7);
+  static const Color red500 = Color(0xff00BEDA);
+  static const Color red400 = Color(0xff00C8DF);
+  static const Color red300 = Color(0xff2FD2E6);
+  static const Color red200 = Color(0xff74E0EE);
+  static const Color red100 = Color(0xffADECF4);
+  static const Color red50 = Color(0xffDEF7FB);
+
+  static const Color yale900 = Color(0xff292D44);
+  static const Color yale800 = Color(0xff343D5D);
+  static const Color yale700 = Color(0xff3B466B);
+  static const Color yale600 = Color(0xff435076);
+  static const Color yale500 = Color(0xff4A577E);
+  static const Color yale400 = Color(0xff636F8F);
+  static const Color yale300 = Color(0xff7E89A2);
+  static const Color yale200 = Color(0xffA1AABD);
+  static const Color yale100 = Color(0xffC5CBD8);
+  static const Color yale50 = Color(0xffE8EAEF);
+
+  static const Color deepBlue900 = Color(0xff00169F);
+  static const Color deepBlue800 = Color(0xff0029B6);
+  static const Color deepBlue700 = Color(0xff0034C4);
+  static const Color deepBlue600 = Color(0xff003ED0);
+  static const Color deepBlue500 = Color(0xff0046DB);
+  static const Color deepBlue400 = Color(0xff0061E9);
+  static const Color deepBlue300 = Color(0xff487DF7);
+  static const Color deepBlue200 = Color(0xff85A2FF);
+  static const Color deepBlue100 = Color(0xffBAC6FF);
+  static const Color deepBlue50 = Color(0xffE4E8FF);
+}
+
+// const Color errorColor = Color(0xFFDC2626); // red-600
+
+// final ColorScheme lightColorScheme = ColorScheme.light(
+//   primary: AppColor.secondary600,
+//   secondary: AppColor.primary400,
+//   onSecondary: Colors.white,
+//   error: errorColor,
+//   background: AppColor.grey10,
+//   onBackground: AppColor.grey10,
+//   onSurface: AppColor.secondary600,
+//   surface: AppColor.secondary100,
+//   surfaceVariant: Colors.white,
+//   shadow: AppColor.secondary500.withOpacity(.1),
+// );
+
+// final ColorScheme darkColorScheme = ColorScheme.dark(
+//   primary: AppColor.primary600,
+//   secondary: AppColor.secondary600,
+//   onSecondary: Colors.white,
+//   error: errorColor,
+//   background: AppColor.secondary900,
+//   onBackground: AppColor.secondary900,
+//   onSurface: AppColor.secondary900,
+//   surface: AppColor.secondary900,
+//   surfaceVariant: AppColor.secondary900,
+//   shadow: AppColor.secondary900.withOpacity(.2),
+// );
+
+// final ThemeData lightTheme = ThemeData(
+//   colorScheme: lightColorScheme,
+//   fontFamily: 'Nunito',
+//   toggleableActiveColor: AppColor.secondary600,
+//   textTheme: const TextTheme(
+//     displayLarge: TextStyle(
+//       color: AppColor.neutrals700,
+//       fontFamily: 'Nunito',
+//     ),
+//     displayMedium: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     displaySmall: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     headlineLarge: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     headlineMedium: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     headlineSmall: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     titleLarge: TextStyle(
+//       color: AppColor.secondary700,
+//       fontFamily: 'Nunito',
+//     ),
+//     titleMedium: TextStyle(
+//       color: AppColor.secondary700,
+//       fontFamily: 'Nunito',
+//     ),
+//     titleSmall: TextStyle(
+//       color: AppColor.secondary700,
+//       fontFamily: 'Nunito',
+//     ),
+//     bodyLarge: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     bodyMedium: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     bodySmall: TextStyle(
+//       color: AppColor.secondary800,
+//       fontFamily: 'Nunito',
+//     ),
+//     labelLarge: TextStyle(
+//       color: AppColor.grey600,
+//       fontFamily: 'Nunito',
+//     ),
+//     labelMedium: TextStyle(
+//       color: AppColor.grey600,
+//       fontFamily: 'Nunito',
+//     ),
+//     labelSmall: TextStyle(
+//       color: AppColor.grey600,
+//       fontFamily: 'Nunito',
+//     ),
+//   ),
+// );
+
+// final ThemeData darkTheme = lightTheme.copyWith(
+//   colorScheme: darkColorScheme,
+//   toggleableActiveColor: AppColor.primary400,
+//   textTheme: const TextTheme(
+//     displayLarge: TextStyle(
+//       color: AppColor.neutral0,
+//       fontFamily: 'Nunito',
+//     ),
+//     displayMedium: TextStyle(
+//       color: AppColor.neutral0,
+//       fontFamily: 'Nunito',
+//     ),
+//     displaySmall: TextStyle(
+//       color: AppColor.neutral0,
+//       fontFamily: 'Nunito',
+//     ),
+//     headlineLarge: TextStyle(
+//       color: AppColor.neutral0,
+//       fontFamily: 'Nunito',
+//     ),
+//     headlineMedium: TextStyle(
+//       color: AppColor.neutral0,
+//       fontFamily: 'Nunito',
+//     ),
+//     headlineSmall: TextStyle(
+//       color: AppColor.neutral0,
+//       fontFamily: 'Nunito',
+//     ),
+//     titleLarge: TextStyle(
+//       color: AppColor.grey10,
+//       fontFamily: 'Nunito',
+//     ),
+//     titleMedium: TextStyle(
+//       color: AppColor.grey10,
+//       fontFamily: 'Nunito',
+//     ),
+//     titleSmall: TextStyle(
+//       color: AppColor.grey10,
+//       fontFamily: 'Nunito',
+//     ),
+//     bodyLarge: TextStyle(
+//       color: AppColor.grey10,
+//       fontFamily: 'Nunito',
+//     ),
+//     bodyMedium: TextStyle(
+//       color: AppColor.grey10,
+//       fontFamily: 'Nunito',
+//     ),
+//     bodySmall: TextStyle(
+//       color: AppColor.grey10,
+//       fontFamily: 'Nunito',
+//     ),
+//     labelLarge: TextStyle(
+//       color: AppColor.grey600,
+//       fontFamily: 'Nunito',
+//     ),
+//     labelMedium: TextStyle(
+//       color: AppColor.grey600,
+//       fontFamily: 'Nunito',
+//     ),
+//     labelSmall: TextStyle(
+//       color: AppColor.grey600,
+//       fontFamily: 'Nunito',
+//     ),
+//   ),
+// );
